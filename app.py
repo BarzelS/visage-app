ad035b1c5143701bcc1e122c4534254e5ed1e23d
Shir <sbarzel@gmail.com>
2024-09-22 22:53:56 +0300

    updates

--- app.py
+++ app.py
@@ -78,6 +78,19 @@ st.markdown(
     unsafe_allow_html=True
 )
 
+st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 st.sidebar.title("About VisageMed")
 st.sidebar.info("""
 VisageMed leverages state-of-the-art deep learning models to assess health risks from facial images. This tool is designed to provide a probability score for diabetes based on facial image analysis.
